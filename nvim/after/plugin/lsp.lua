8170bbcd831ec4428dca07a2ebf69f509168f359
Mate Czagany <czmate10@gmail.com>
2023-12-14 18:07:48 +0100

    stuff

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -43,6 +43,11 @@ end)
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+-- Setup clangd
+require('lspconfig').clangd.setup({
+    cmd={'/usr/bin/clangd'},
+})
+
 -- GROUPS:
 local disable_node_modules_eslint_group =
 	vim.api.nvim_create_augroup("DisableNodeModulesEslint", { clear = true })
