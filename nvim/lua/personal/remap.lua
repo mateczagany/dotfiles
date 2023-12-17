535164239f04a7f8f4d7835765673264c07323c2
Mate Czagany <czmate10@gmail.com>
2023-12-17 09:13:06 +0100

    Fix remap.lua

--- nvim/lua/personal/remap.lua
+++ nvim/lua/personal/remap.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", ":Ex<cr>")
 
 -- Move lines in visual mode, auto-indent
 vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
