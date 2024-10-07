70d7011441e711d2179b17a160690b33015b5618
mateczagany <czmate10@gmail.com>
2024-10-07 15:07:23 +0200

    Add better-escape

--- nvim/lua/personal/plugins.lua
+++ nvim/lua/personal/plugins.lua
@@ -14,6 +14,12 @@ vim.opt.rtp:prepend(lazypath)
 
 return {
     {
+      "max397574/better-escape.nvim",
+      config = function()
+        require("better_escape").setup()
+      end,
+    },
+    {
       "christoomey/vim-tmux-navigator",
       cmd = {
         "TmuxNavigateLeft",
