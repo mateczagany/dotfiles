bb01593053bb2f4b0601b9d865131bfade0aa429
mateczagany <czmate10@gmail.com>
2024-11-26 14:11:45 +0100

    Add helm-ls

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
         },
         -- import/override with your plugins
         { import = "lazyvim.plugins.extras.lang.rust" },
+        { import = "lazyvim.plugins.extras.lang.helm" },
         {
             import = "lazyvim.plugins.extras.test.core",
             opts = {
