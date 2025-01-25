8e7ab321ba6a3bd26ef7c4f954d476c517e8438c
Mate Czagany <czmate10@gmail.com>
2025-01-25 09:17:24 +0100

    Use catppuccin mocha

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
             "LazyVim/LazyVim",
             import = "lazyvim.plugins",
             opts = {
-                colorscheme = "catppuccin",
+                colorscheme = "catppuccin-mocha",
             },
         },
         -- Disable snacks dashboard
